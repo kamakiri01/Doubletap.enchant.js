0d29fca8419c3327669fa87de2dda06c7d301c42
kamakiri01 <kamakiri0148@gmail.com>
2015-03-18 16:51:46 +0900

    fix API, dispatch event to all childNodes.

--- doubletap.enchant.js
+++ doubletap.enchant.js
@@ -1,22 +1,24 @@
-// setter.setDoubleTapHandler(enchant.Core.instance.rootScene)
-var setter = (function(){
-        enchant.annex = {
-            _env: {
-                HOLDTIME: 300,
-                DBLLIMIT: 300,
-                FLINGVEL: 3
-            },
-            Event: {}
+// enchant.annex.Doubletap.setDoubleTapHandler(enchant.Core.instance.rootScene)
+if(enchant.annex === undefined){
+    enchant.annex = {};
+}
+enchant.annex.DoubleTap = (function(){
+        var _env = {
+            HOLDTIME: 300,
+            DBLLIMIT: 300,
+            FLINGVEL: 3
         };
-        enchant.annex.Event.DOUBLETAP = 'doubletap';
+        var Event = {
+            DOUBLETAP: 'doubletap'
+        };
         var NOTOUCH = 0;
         var WAITDBL = 1;
         var NOMOVE = 2;
         var NOMOVEDBL = 3;
         var MOVED = 4;
         var HOLD = 5;
-        enchant.annex.DoubhleTapDetector = enchant.Class.create(enchant.EventTarget, {
-                initialize: function(){
+        var DoubhleTapDetector = enchant.Class.create(enchant.EventTarget, {
+                initialize: function(target){
                     var core = enchant.Core.instance;
                     enchant.EventTarget.call(this);
                     this._target;
@@ -27,10 +29,17 @@ var setter = (function(){
                     this._touchElapsed = 0;
                     this._releaseElapsed = 0;
                     this._state = NOTOUCH;
+                    if(target){
+                        this.attach(target);
+                    }
+                },
+                attach: function(target) {
+                    this._target = target;
+                    //this._types.forEach(function(event) {
+                    //        this._target.addEventListener(event, this._handler);
+                    //}, this);
                 },
                 ontouchstart: function(e) {
-                    var core = enchant.Core.instance;
-                    // this._startFrame = core.frame;
                     this._startX = this._lastX = e.x;
                     this._startY = this._lastY = e.y;
                     if (this._state == WAITDBL) {
@@ -69,13 +78,12 @@ var setter = (function(){
                     }
                 },
                 ontouchend: function(e) {
-                    console.log("touchend");
                     var core = enchant.Core.instance;
                     switch (this._state) {
                         case MOVED:
                         velocityX = (this._lastX - this._startX) / this._velobase / this._touchElapsed * 1000;
                         velocityY = (this._lastY - this._startY) / this._velobase / this._touchElapsed * 1000;
-                        if (velocityX > enchant.annex._env.FLINGVEL || velocityY > enchant.annex._env.FLINGVEL) {
+                        if (velocityX > enchant.annex.DoubleTap._env.FLINGVEL || velocityY > enchant.annex.DoubleTap._env.FLINGVEL) {
                             // var evt = new enchant.Event(enchant.Event.FLING);
                             // evt.x = this._startX;
                             // evt.y = this._startY;
@@ -100,6 +108,11 @@ var setter = (function(){
                         // evt.x = this._lastX;
                         // evt.y = this._lastY;
                         // this._target.dispatchEvent(evt);
+                        var evt = new enchant.Event('doubletap');
+                        evt.x = this._lastX;
+                        evt.y = this._lastY;
+                        this._target.dispatchEvent(evt);
+
                         this._state = NOTOUCH;
                         this._releaseElapsed = 0;
                         break;
@@ -119,7 +132,7 @@ var setter = (function(){
                     switch (this._state) {
                         case WAITDBL:
                         this._releaseElapsed += elapsed;
-                        if (this._releaseElapsed >= enchant.annex._env.DBLLIMIT) {
+                        if (this._releaseElapsed >= enchant.annex.DoubleTap._env.DBLLIMIT) {
                             // var evt = new enchant.Event(enchant.Event.TAP);
                             // evt.x = this._lastX;
                             // evt.y = this._lastY;
@@ -132,13 +145,13 @@ var setter = (function(){
                         break;
                         case NOMOVEDBL:
                         this._releaseElapsed += elapsed;
-                        if (this._releaseElapsed >= enchant.annex._env.DBLLIMIT) {
+                        if (this._releaseElapsed >= enchant.annex.DoubleTap._env.DBLLIMIT) {
                             this._state = NOMOVE;
                             this._releaseElapsed = 0;
                         }
                         case NOMOVE:
                         this._touchElapsed += elapsed;
-                        if (this._touchElapsed >= enchant.annex._env.HOLDTIME) {
+                        if (this._touchElapsed >= enchant.annex.DoubleTap._env.HOLDTIME) {
                             // var evt = new enchant.Event(enchant.Event.HOLD);
                             // evt.x = this._lastX;
                             // evt.y = this._lastY;
@@ -158,8 +171,24 @@ var setter = (function(){
                 }
         });
         var setDoubleTapHandler = function(scene){
-            //拾いたいイベントごとに全部リスナーを列挙？
-            var hander = new enchant.annex.DoubhleTapDetector();
+            var hander = new DoubhleTapDetector(scene);
+
+            scene.addEventListener('doubletap', function(e){
+                    var evt = new enchant.Event('doubletap');
+                    var nodes = scene.childNodes.slice();
+                    var push = Array.prototype.push;
+                    while (nodes.length) {
+                        var node = nodes.pop();
+                        node.dispatchEvent(e);
+                        if (node.childNodes) {
+                            push.apply(nodes, node.childNodes);
+                        }
+                    }
+            });
+            
+
+
+
             scene.addEventListener('enterframe', function(e){
                 hander.dispatchEvent(e);
             });
@@ -173,7 +202,11 @@ var setter = (function(){
                 hander.dispatchEvent(e);
             });
         };
-        return {setDoubleTapHandler: setDoubleTapHandler};
+        return {
+            setDoubleTapHandler: setDoubleTapHandler,
+            _env: _env,
+            Event, Event
+        };
 })();
 
 
